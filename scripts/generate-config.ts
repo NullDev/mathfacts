99b4bbf11a0f47af9bae13aed87147c324d31a50
NullDev (Shadow) <chris@nulldev.org>
2026-03-16 21:48:00 +0100

    seed & config gen

---
+++ scripts/generate-config.ts
@@ -0,0 +1,12 @@
+import fs from "node:fs/promises";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// =     - SPDX: MIT -     = //
+// ========================= //
+
+if (await fs.stat("./config/config.custom.js").catch(() => false)){
+    console.log("Config file already exists. Skipping...");
+    process.exit(0);
+}
+await fs.copyFile("./config/config.template.ts", "./config/config.custom.ts");
